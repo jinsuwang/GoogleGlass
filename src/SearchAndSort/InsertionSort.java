727e8ce4d9dadbea77b48bf6c1e3f05556b8fece
Su Jin <jimmyjin616@gmail.com>
2016-04-23 15:29:12 -0700

    insertion sort

---
+++ src/SearchAndSort/InsertionSort.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SearchAndSort;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Sam
+ */
+public class InsertionSort {
+    
+    // 
+    
+    public static void myInsertionSort( int[] nums ){
+        int tmp;
+        for( int i=1; i < nums.length; i++ ){
+            for( int j=i; j>0; j--){
+                if( nums[j-1] > nums[j]){
+                    tmp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = tmp; 
+                }
+                System.out.println( Arrays.toString(nums) );
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] test = { 5,4,3,2,1 };
+        myInsertionSort( test );
+        System.out.println(Arrays.toString(test));
+    }
+}
