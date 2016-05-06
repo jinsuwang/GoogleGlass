d87595a8dfbca024c1e0138e19c10829f89b4ed2
Su Jin <jimmyjin616@gmail.com>
2016-05-06 15:13:08 -0700

    flip game II

--- src/DFSBFS/FlipGameII.java
+++ src/DFSBFS/FlipGameII.java
@@ -14,14 +14,14 @@ import java.util.List;
  */
 public class FlipGameII {
     
-    public static boolean canWin(String s) {
+    public static boolean canWin(String s, int level ) {
         List<String> tmp = new ArrayList<String>();
         for( int i = 0; i < s.length()-1; i++ ){
             if(s.substring(i,i+2).equals("++"))
                 tmp.add( s.substring(0,i) + "--"+ s.substring(i+2) );
         }
         
-        System.out.println(tmp.toString());
+//        System.out.println(" Level: " + level + " prev: "+ s + tmp.toString());
         
         for( String e : tmp ){
             if( !canWin(e) ){
@@ -33,7 +33,7 @@ public class FlipGameII {
     
     public static void main(String[] args) {
         
-        System.out.println( canWin("++++++") );
+        System.out.println( canWin("+--+", 0) );
         
     }
     
