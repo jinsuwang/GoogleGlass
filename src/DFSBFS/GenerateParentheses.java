ac4bb227530bac8896a270ce5e180ac73d5d8b4e
Su Jin <jimmyjin616@gmail.com>
2016-05-06 00:45:41 -0700

    generate parenthesis

---
+++ src/DFSBFS/GenerateParentheses.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DFSBFS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Sam
+ */
+public class GenerateParentheses {
+    
+    public List<String> generateParenthesis(int n) {
+        
+        ArrayList<String> result = new ArrayList<String>();
+        if (n <= 0) {
+            return result;
+        }
+        dfs(result, "", n, n);
+        return result;
+    }
+    
+    public void dfs(ArrayList<String> result, String paren, // current paren
+            int left, int right) {  // how many right paren we need to add
+	if (left == 0 && right == 0) {
+            result.add(paren);
+            return;
+	}
+        if (left > 0) { 
+            dfs(result, paren + "(", left - 1, right);
+        }
+        
+        if (right > 0 && left < right) {
+            dfs(result, paren + ")", left, right - 1);
+        }
+    }
+}
