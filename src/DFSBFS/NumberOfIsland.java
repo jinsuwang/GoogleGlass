9fd43fcfbf553c02887bbd4fe6c89ad25d09b06e
Su Jin <jimmyjin616@gmail.com>
2016-05-16 01:14:31 -0700

    expression

--- src/DFSBFS/NumberOfIsland.java
+++ src/DFSBFS/NumberOfIsland.java
@@ -5,6 +5,8 @@
  */
 package DFSBFS;
 
+import java.util.Stack;
+
 /**
  *
  * @author Sam
@@ -36,4 +38,54 @@ public class NumberOfIsland {
             dfs( grid, i,j-1);
         }
     }
+    
+    
+    public static int numIslands2(int[][] grid) { 
+        int m = grid.length;  if( m == 0 ) return 0;
+        int n = grid[0].length; if( n == 0 ) return 0;
+        int ret = 0;
+        int max = Integer.MIN_VALUE;
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        int[] dirX = { 1, -1,  0, 0 };
+        int[] dirY = { 0,  0, -1, 1 };
+
+        for( int i = 0; i < m; i++ ){
+            for( int j = 0; j < n; j++ ){
+                if( grid[i][j] <= 0 ) continue;
+                ret++;
+                int curr = grid[i][j];
+                grid[i][j] = -1;
+                stack.push( i * n + j);
+                while( !stack.isEmpty() ){
+                    int tmp = stack.pop();
+                    int x = tmp / n;
+                    int y = tmp % n;
+//                    System.out.println("x " + x + " y "+ y );
+                    for( int k = 0; k < dirX.length; k++ ){
+                        int newX = x+dirX[k]; int newY = y+dirY[k]; 
+                        if( newX >= 0 && newX < m && newY >= 0 && newY < n 
+                                && grid[newX][newY] > 0  ){
+                            curr += grid[newX][newY];
+                            grid[newX][newY] = -1;
+                            stack.push( newX * n + newY );
+                        }
+                    }
+                }
+                max = Math.max(max, curr);
+            }
+        }
+        return max;
+    } 
+    
+    public static void main(String[] args) {
+        int[][] world = {
+            { 1, 2, 11 },
+            { 5, 0,  0 },
+            { 0, 0, 13 }
+        };
+        System.out.println( numIslands2( world ));
+    }
+    
 }
---
+++ src/StackAndQueue/ShuntingYang.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StackAndQueue;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Sam
+ */
+public class ShuntingYang {
+    
+    public int evaluate( String str ){
+        char[] tokens = str.toCharArray();
+        
+        Stack<Integer> values = new Stack<Integer>();
+        Stack<Character> ops = new Stack<Character>();
+        
+        for( int i=0; i < tokens.length; i++ ){
+            if(tokens[i] == ' ') continue;
+            if (tokens[i] >= '0' && tokens[i] <= '9')
+            {
+                StringBuffer sbuf = new StringBuffer();
+                // There may be more than one digits in number
+                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
+                    sbuf.append(tokens[i++]);
+                values.push(Integer.parseInt(sbuf.toString()));
+            }
+            
+            else if (tokens[i] == '(')
+                ops.push(tokens[i]);
+            
+            else if (tokens[i] == ')')
+            {
+                while (ops.peek() != '(')
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                ops.pop(); // remove '('
+            }
+            
+            else if (tokens[i] == '+' || tokens[i] == '-' ||
+                     tokens[i] == '*' || tokens[i] == '/')
+            {
+                // While top of 'ops' has same or greater precedence to current
+                // token, which is an operator. Apply operator on top of 'ops'
+                // to top two elements in values stack
+                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+ 
+                // Push current token to 'ops'.
+                ops.push(tokens[i]);
+            }
+        }
+        while (!ops.empty())
+            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+        return values.pop();
+    }
+
+    private Integer applyOp(Character op, Integer a, Integer b) {
+        switch (op)
+        {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            if (b == 0)
+                throw new
+                UnsupportedOperationException("Cannot divide by zero");
+            return a / b;
+        }
+        return 0;
+    }
+
+    private boolean hasPrecedence(char op1, Character op2) {
+        if (op2 == '(' || op2 == ')')
+            return false;
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+            return false;
+        else
+            return true;
+    }
+}
